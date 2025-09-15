2e4374bcb5b4630cb53361706a0797e13cf9c5ff
Sanathsunny969 <Sanathkumarbode@gmail.com>
2025-09-14 21:16:30 -0400

    CICD updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Website",
+  base: "/Personal-Website/",
+  server:{open:true,
+  port:3001,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
